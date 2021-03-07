fdbf5364064b477bb80901d68c9a1863b9387738
LordKBX <kevboulain@aol.com>
2021-03-07 01:48:05 +0100

    new build files

--- installer/test2.bat
+++
@@ -1,8 +0,0 @@
-SETLOCAL ENABLEDELAYEDEXPANSION
-python -V > ./fool.txt
-set /p PythonVer= < ./fool.txt
-del ./fool.txt
-SET PythonVer=!PythonVer:Python=!
-SET PythonVer=!PythonVer: =!
-echo %PythonVer%
-ENDLOCAL
